96b18db44ce63349fc9af6a6ed223a47e207c3b2
Erin Holmes <e.holmes09@yahoo.com>
2021-07-15 16:40:51 -0500

    add page url  routes to animals and zookeepers html files

--- server.js
+++ server.js
@@ -96,6 +96,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
