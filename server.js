03ec830b0fbf4f62b5af3ec0e390a9f71eb0e5bf
Erin Holmes <e.holmes09@yahoo.com>
2021-07-15 16:17:06 -0500

    set up route for index.html and middleware to read css & js files in public folder

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
 
 const { animals } = require("./data/animals.json");
 
@@ -90,6 +91,10 @@ function createNewAnimal(body, animalsArray) {
   // return finished code to post route for response
   return body;
 }
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
