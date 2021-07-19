ed1ff3884285a5357ba6084c236945735d3ba377
Erin Holmes <e.holmes09@yahoo.com>
2021-07-19 15:27:33 -0500

    move routing files and start adding tests, creating new branch to finish tests

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // parse incoming string or array data
@@ -10,55 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const { animals } = require("./data/animals.json");
-
-app.get("/api/animals", (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/animals.html"));
-});
-
-app.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/api/animals/:id", (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  } 
-});
-
-app.post("/api/animals", (req, res) => {
-  // req.body is where our incoming content will be
-  console.log(req.body);
-  // set id based on the next index of the array
-  req.body.id = animals.length.toString();
-
-  if (!validateAnimal(req.body)) {
-    res.status(400).send("The animal is not properly formatted.");
-  } else {
-    // add animal to json file and animals array
-    const animal = createNewAnimal(req.body, animals);
-    res.json(animal);
-  } 
-});
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
   console.log( `API server now on port ${PORT}!`);
